39617d140bcd6f331667df5ed7b134e2c73a4f5a
StageGuard <1355416608@qq.com>
2024-02-04 18:59:31 +0800

    move framebuffer logger initializer to mod

--- kernel/src/logger/mod.rs
+++ kernel/src/logger/mod.rs
@@ -1,8 +1,14 @@
-use shared::{framebuffer::Framebuffer, framebuffer_writer::FrameBufferWriter};
+use log::Log;
+use shared::{framebuffer::Framebuffer, framebuffer_writer::FrameBufferWriter, uni_processor::UPSafeCell};
 use spin::Mutex;
-use core::{fmt::Write, slice};
+use core::{fmt::Write, mem::MaybeUninit, slice};
+use lazy_static::lazy_static;
 
-use crate::device::qemu::exit_qemu;
+use crate::{device::qemu::exit_qemu, qemu_println};
+
+lazy_static! {
+    static ref FRAMEBUFFER_LOGGER: UPSafeCell<MaybeUninit<FramebufferLogger<'static>>> = unsafe { UPSafeCell::new(MaybeUninit::uninit()) };
+}
 
 pub struct FramebufferLogger<'a> {
     writter: Mutex<FrameBufferWriter<'a>>,
@@ -30,4 +36,15 @@ impl log::Log for FramebufferLogger<'_> {
     fn flush(&self) {
         
     }
+}
+
+pub fn initialize_framebuffer_logger(framebuffer: &'static Framebuffer) {
+    let mut logger = FRAMEBUFFER_LOGGER.inner_exclusive_mut();
+    let logger_ref = logger.write(FramebufferLogger::new(framebuffer));
+
+    if let Err(err) = log::set_logger(unsafe { &*(logger_ref as *const dyn Log) }) {
+        qemu_println!("kernel failed to initialize framebuffer logger: {}", err);
+        exit_qemu(crate::device::qemu::QemuExitCode::Success);
+    };
+    log::set_max_level(log::LevelFilter::Debug);
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -10,7 +10,7 @@ use log::{info, Log};
 use shared::{arg::KernelArg, framebuffer::{FBPixelFormat, Framebuffer}, uni_processor::UPSafeCell};
 use spin::mutex::Mutex;
 
-use crate::logger::FramebufferLogger;
+use crate::logger::{initialize_framebuffer_logger, FramebufferLogger};
 
 extern crate alloc;
 
@@ -21,17 +21,17 @@ mod logger;
 
 lazy_static! {
     static ref FRAMEBUFFER: UPSafeCell<Mutex<MaybeUninit<Framebuffer>>> = unsafe { UPSafeCell::new(Mutex::new(MaybeUninit::uninit())) };
-    static ref FRAMEBUFFER_LOGGER: UPSafeCell<MaybeUninit<FramebufferLogger<'static>>> = unsafe { UPSafeCell::new(MaybeUninit::uninit()) };
 }
 
 #[no_mangle]
 pub extern "C" fn _start(arg: &'static KernelArg) -> ! {
+
     {
         // initialize framebuffer
         let framebuffer_mutex = FRAMEBUFFER.inner_exclusive_mut();
         let mut framebuffer = framebuffer_mutex.lock();
         
-        framebuffer.write(Framebuffer::new(
+        let fb_ref = framebuffer.write(Framebuffer::new(
             arg.framebuffer_addr as *mut u8, 
             arg.framebuffer_len, 
             arg.framebuffer_width, 
@@ -40,23 +40,12 @@ pub extern "C" fn _start(arg: &'static KernelArg) -> ! {
             FBPixelFormat::RGB
         ));
 
-        let mut logger = FRAMEBUFFER_LOGGER.inner_exclusive_mut();
-        let logger_ref = logger.write(
-            FramebufferLogger::new(unsafe { &*(&framebuffer.assume_init() as *const Framebuffer) })
-        );
-
-        if let Err(err) = log::set_logger(unsafe { &*(logger_ref as *const dyn Log) }) {
-            
-        };
-        log::set_max_level(log::LevelFilter::Debug);
-
+        initialize_framebuffer_logger(unsafe { &*(fb_ref as *const Framebuffer) });
+        info!("kernel framebuffer logger is initialized.");
     }
     
 
-    info!("kernel framebuffer logger is initialized.");
     halt();
-
-
 }
 
 fn halt() -> ! {
